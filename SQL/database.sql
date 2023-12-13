97d80a67bae920381c7199e9e6f46499233de164
HGThinh <132908713+HGThinh@users.noreply.github.com>
2023-12-13 14:07:59 +0100

    Create table building
    Create table building with rows: (PK)building_id, building_name, building_cost, upgrade_cost

--- SQL/database.sql
+++ SQL/database.sql
@@ -1,5 +1,12 @@
 CREATE DATABASE IF NOT EXISTS `etherealhyperspacebattleshipsdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `etherealhyperspacebattleshipsdb`;
 
+CREATE TABLE IF NOT EXISTS `building` (
+  `building_id` int(11) NOT NULL AUTO_INCREMENT,
+  `building_name` varchar(255) NOT NULL,
+  `building_cost` int(11) NOT NULL,
+  `upgrade_cost` int(11) NOT NULL,
+  PRIMARY KEY (`building_id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
