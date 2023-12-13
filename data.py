1ac4952eb7b3be79df7bd979f487b612621c85a2
IGLADI <jarno.roels@gmail.com>
2023-12-13 22:19:02 +0100

    created an energy regen system (you still never loose energy)
    implemented add&remove energy&resources, changes ship cogs to also work with uranium & energy

--- data.py
+++ data.py
@@ -1,4 +1,9 @@
 # TODO this is a temporary file to store data, will be replaced by a database/json file
+class Guild:
+    def __init__(self, name):
+        self.name = name
+
 
 race_games = {}
 players = {}
+guilds = [Guild("The Federation"), Guild("The Empire"), Guild("The Alliance"), Guild("The Independents")]
--- ship.py
+++ ship.py
@@ -14,6 +14,17 @@ class Ship:
         self._modules.append(Cargo())
         self._modules.append(Radar())
         self._modules.append(Energy_Generator())
+        # TODO make this in apart battery module
+        self._energy = 100
+        self._energy_capacity = 100
+
+    @property
+    def energy_capacity(self):
+        return self._energy_capacity
+
+    @property
+    def energy(self):
+        return self._energy
 
     @property
     def modules(self):
@@ -22,3 +33,21 @@ class Ship:
     @property
     def location(self):
         return self._location
+
+    def add_energy(self, amount):
+        self._energy += amount
+
+    def remove_energy(self, amount):
+        self._energy -= amount
+
+    def remove_resource(self, resource_name, amount):
+        for resource in self.modules[5]._capacity:
+            if resource.name == resource_name:
+                resource.amount -= amount
+                break
+
+    def add_resource(self, resource_name, amount):
+        for resource in self.modules[5]._capacity:
+            if resource.name == resource_name:
+                resource.amount += amount
+                break
