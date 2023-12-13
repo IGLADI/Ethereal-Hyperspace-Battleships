1ceab85af66775a6bd5885577430c2ef1650f244
HGThinh <132908713+HGThinh@users.noreply.github.com>
2023-12-13 16:41:24 +0100

    Update database.py
    Make function to get connection and insert data

--- database.py
+++ database.py
@@ -1,33 +1,51 @@
 import pymysql
 import sys
 
-try:
-    connection = pymysql.connect(host="localhost", user="root", password="groupx", database="etherealhyperspacebattleshipsdb")
-    cursor = connection.cursor()
-except Exception as e:
-    print("Error connecting to MariaDB:", e)
-    sys.exit(1)
+# Make a function to insert_data into database
+def insert_data(connection, table_name, column_names, values):
+    # Prepare the INSERT statement with placeholders for the values
+    insert_statement = f"INSERT INTO {table_name}({','.join(column_names)}) VALUES (%s, %s, ...);"
+    
+    # Prepare the tuple of values to be inserted
+    values_tuple = tuple(values)
 
-# Prompt the user for the table name
-table_name = input("Enter the table name: ")
+    # Execute the INSERT statement and commit the changes
+    try:
+        cursor.execute(insert_statement, values_tuple)
+        connection.commit()
+        print("Data inserted successfully.")
+    except Exception as e:
+        connection.rollback()
+        print("Error inserting data:", e)
 
-# Prompt the user for the column names and values
-column_names = input("Enter the column names separated by commas: ")
-column_names = column_names.split(", ")
-values = input("Enter the corresponding values separated by commas: ")
-values = values.split(", ")
+def get_database_connection():
+    try:
+        # Connect to MariaDB server
+        connection = pymysql.connect(host="localhost", user="root", password="projectx", database="etherealhyperspacebattleshipsdb")
+        cursor = connection.cursor()
 
-# Prepare the INSERT statement with placeholders for the values
-insert_statement = f"INSERT INTO {table_name}({','.join(column_names)}) VALUES (%s, %s, ...);"
+        # Return the connection object
+        return connection
+    except Exception as e:
+        # Print error message
+        print("Error connecting to MariaDB:", e)
+        return None
 
-# Prepare the tuple of values to be inserted
-values_tuple = tuple(values)
-try:
-    cursor.execute(insert_statement, values_tuple)
-    connection.commit()
-    print("Data inserted successfully.")
-except Exception as e:
-    connection.rollback()
-    print("Error inserting data:", e)
+if __name__ == "__main__":
+    try:
+        get_database_connection()
 
-connection.close()
+        # Get the table column names and values from the user
+        table_name = input("Enter the table name: ")
+        column_names = input("Enter the column names separated by commas: ")
+        column_names = column_names.split(", ")
+        values = input("Enter the corresponding values separated by commas: ")
+        values = values.split(", ")
+
+        # Call the function to insert data into the specified table
+        insert_data(connection, table_name, column_names, values)
+
+        connection.close()
+    except Exception as e:
+        print("Error connecting to MariaDB:", e)
+        sys.exit(1)
