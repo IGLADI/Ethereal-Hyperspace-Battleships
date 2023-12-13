b6b15074717bc3ab15a9a144b8103046c638f19c
HGThinh <132908713+HGThinh@users.noreply.github.com>
2023-12-13 14:16:37 +0100

    Create table location
    Create table location with rows: (PK)location_id, x_pos, y_pos, location_description

--- SQL/database.sql
+++ SQL/database.sql
@@ -18,3 +18,14 @@ CREATE TABLE IF NOT EXISTS `guild` (
   `members` text NOT NULL,
   PRIMARY KEY (`guild_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `location` (
+  `location_id` int(11) NOT NULL AUTO_INCREMENT,
+  `x_pos` int(11) NOT NULL,
+  `y_pos` int(11) NOT NULL,
+  `location_description` varchar(255) NOT NULL,
+  PRIMARY KEY (`location_id`),
+  UNIQUE KEY `x_pos` (`x_pos`) USING BTREE,
+  UNIQUE KEY `y_pos` (`y_pos`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
