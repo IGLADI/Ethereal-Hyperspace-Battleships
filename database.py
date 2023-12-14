a2f7cb00536fbdaa39fcd751cf30e11ae81b74b2
HGThinh <132908713+HGThinh@users.noreply.github.com>
2023-12-14 10:29:32 +0100

    Change from mysql to maraiadb
    Change the code from using with mysql to mariadb

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
-import pymysql
+import mariadb
 import sys
 
 # Make a function to insert_data into database
 def insert_data(connection, table_name, column_names, values):
     # Prepare the INSERT statement with placeholders for the values
-    insert_statement = f"INSERT INTO {table_name}({','.join(column_names)}) VALUES (%s, %s, ...);"
+    insert_statement = f"INSERT INTO {table_name}({','.join(column_names)}) VALUES (?, ?, ...);"
     
     # Prepare the tuple of values to be inserted
     values_tuple = tuple(values)
@@ -21,14 +21,20 @@ def insert_data(connection, table_name, column_names, values):
 def get_database_connection():
     try:
         # Connect to MariaDB server
-        connection = pymysql.connect(host="localhost", user="root", password="projectx", database="etherealhyperspacebattleshipsdb")
+        connection = mariadb.connect(
+            host="localhost", 
+            user="root", 
+            password="projectx", 
+            host="192.0.2.1", port=3306, 
+            database="etherealhyperspacebattleshipsdb")
         cursor = connection.cursor()
 
         # Return the connection object
         return connection
-    except Exception as e:
+    except mariadb.Error as e:
         # Print error message
         print("Error connecting to MariaDB:", e)
+        sys.exit(1)
         return None
 
 if __name__ == "__main__":
