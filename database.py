fc3b12b422d37f7981e03d5964085403dbc78758
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-12-16 01:45:12 +0100

    feat(db): use credentials stored in config.json

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import mariadb
+import json
 
 
 # Make a function to insert_data into database
@@ -8,33 +9,24 @@ def insert_data(connection, table_name, column_names, values):
     
     # Prepare the tuple of values to be inserted
     values_tuple = tuple(values)
-
-    # Execute the INSERT statement and commit the changes
-    try:
-        cursor.execute(insert_statement, values_tuple)
-        connection.commit()
-        print("Data inserted successfully.")
-    except Exception as e:
-        connection.rollback()
-        print("Error inserting data:", e)
-
 def get_database_connection():
+    """Returns a connection to the database using credentials in config.json"""
+    with open("config.json", "r") as f:
+        data = json.load(f)
+        db_data = data["database"]
+
+    host = db_data["host"]
+    user = db_data["user"]
+    password = db_data["password"]
+    database = db_data["database"]
+
     try:
-        # Connect to MariaDB server will take login information from config.json
         connection = mariadb.connect(
-            host="", 
-            user="", 
-            password="", 
-            host="", 
-            port=, 
-            database="")
+            host=host, user=user, password=password, port=3306, database=database
+        )
         cursor = connection.cursor()
 
-        # Return the connection object
         return connection
     except mariadb.Error as e:
-        # Print error message
         print("Error connecting to MariaDB:", e)
         return None
-
-
