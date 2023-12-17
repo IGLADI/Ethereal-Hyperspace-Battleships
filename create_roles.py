b18ac9f9ea42a367719300e14cdf3fd9516a4266
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-12-17 07:24:44 +0100

    fix: use guild_names instead of objects
    The objects should not have been in data.py in the first place.
    guild_names is a more fitting name

--- create_roles.py
+++ create_roles.py
@@ -3,8 +3,8 @@ import discord
 
 
 async def create_roles(guild: discord.Guild):
-    for guild_temp in data.guilds:
-        if not discord.utils.get(guild.roles, name=guild_temp.name):
-            await guild.create_role(name=guild_temp.name)
-        if not discord.utils.get(guild.roles, name=f"{guild_temp.name} officer"):
-            await guild.create_role(name=f"{guild_temp.name} officer")
+    for guild_name in data.guild_names:
+        if not discord.utils.get(guild.roles, name=guild_name):
+            await guild.create_role(name=guild_name)
+        if not discord.utils.get(guild.roles, name=f"{guild_name} officer"):
+            await guild.create_role(name=f"{guild_name} officer")
--- data.py
+++ data.py
@@ -1,13 +1,10 @@
 # TODO this is a temporary file to store data, will be replaced by a database/json file
-class Guild:
-    def __init__(self, name):
-        self.name = name
-
 
 race_games = {}
 players = {}
 planets = {}
-guilds = [Guild("The Federation"), Guild("The Empire"), Guild("The Alliance"), Guild("The Independents")]
+
+guild_names = ["The Federation", "The Empire", "The Alliance", "The Independents"]
 
 guild_channels = [
     {"type": "text", "name": "announcements"},
