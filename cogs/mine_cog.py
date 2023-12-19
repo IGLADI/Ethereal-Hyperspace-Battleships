a7decf33bb1ae671e28c1bcb76e1ba9ce284ef80
ThomasKnoops <56110999+ThomasKnoops@users.noreply.github.com>
2023-12-19 14:58:00 +0100

    feat: added mining_sessions to /mining

--- cogs/mine_cog.py
+++ cogs/mine_cog.py
@@ -25,26 +25,27 @@ class MineCommands(commands.Cog):
 
     # Chances of getting a resource:
     # Copper: 35% | Silver: 30% |Gold: 25% | Uranium: 7% | Black Matter: 3%
-    # TODO mine X times (avoid spamming /mine)
     @app_commands.command(name="mine", description="Mine a random resource")
     @app_commands.check(check_registered)
-    async def mine(self, interaction: discord.Interaction):
+    async def mine(self, interaction: discord.Interaction, mining_sessions: int = 1):
         player = Player.get(interaction.user.id)
-        if player.ship.energy < 10:
+        if player.ship.energy < 10 * mining_sessions:
             await interaction.response.send_message(
                 "You don't have enough energy.", ephemeral=True
             )
             return
 
-        # TODO mining module changes energy efficiency
-        player.ship.energy -= 10
-        mining_bonus = player.ship.modules["MiningModule"].mining_bonus
-        resource_name = random.choices(RESOURCE_NAMES, weights=[45, 30, 20, 3, 2, 1])[0]
-        amount = math.floor((random.random() * mining_bonus) / 2)
-        player.ship.modules["Cargo"].add_resource(resource_name, amount)
-        await interaction.response.send_message(
-            f"You mined {amount} tons of {resource_name}.", ephemeral=True
-        )
+        await interaction.response.send_message("Mining...", ephemeral=True)
+        for i in range(mining_sessions):
+            # TODO mining module changes energy efficiency
+            player.ship.energy -= 10
+            mining_bonus = player.ship.modules["MiningModule"].mining_bonus
+            resource_name = random.choices(RESOURCE_NAMES, weights=[45, 30, 20, 3, 2, 1])[0]
+            amount = math.floor((random.random() * mining_bonus) / 2)
+            player.ship.modules["Cargo"].add_resource(resource_name, amount)
+            await interaction.followup.send(
+                f"You mined {amount} tons of {resource_name}.", ephemeral=True
+            )
 
 
 async def setup(client: commands.Bot) -> None:
